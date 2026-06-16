9468151a6e79f94e728a1d1eab54b4f084b838f9
Peter Chege <roychegeh@gmail.com>
2026-06-16 10:03:34 +0300

    configure changeNotifier and provider widgets

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,28 +1,41 @@
 
 import 'package:flutter/material.dart';
+import 'package:frivia/providers/game_page_provider.dart';
+import 'package:provider/provider.dart';
 
 class GamePage extends StatelessWidget {
   double? _deviceHeight, _deviceWidth;
+
+  GamePageProvider? _pageProvider;
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
 
-    return _buildUI();
+    return ChangeNotifierProvider(
+        create: (_context) => GamePageProvider(context: context),
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      body: SafeArea(
-          child: Container(
-            padding: EdgeInsets.symmetric(
-                horizontal: _deviceHeight! * 0.05
-            ),
+    return Builder(
+      builder: (_context) {
+        _pageProvider = _context.watch<GamePageProvider>();
+        
+        return Scaffold(
+          body: SafeArea(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: _deviceHeight! * 0.05
+                ),
 
-            child: _gameUI(),
+                child: _gameUI(),
+              ),
           ),
-      ),
+        );
+      }
     );
   }
 
---
+++ lib/providers/game_page_provider.dart
@@ -0,0 +1,14 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class GamePageProvider extends ChangeNotifier {
+  final Dio _dio = Dio();
+
+  BuildContext context;
+
+  GamePageProvider({required this.context}) {
+    _dio.options.baseUrl = 'https://opentbd.com/api.php';
+    print("hello");
+  }
+}
